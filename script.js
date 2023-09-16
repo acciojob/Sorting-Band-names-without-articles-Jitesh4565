49a7f94bec3d9c2ecc38878fbc3a6a3abd7ed5e8
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-09-16 11:42:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
-
+let regexp = /\bthe\b|\ban\b|\ba\b/gi;
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let articlelessarr=[];
+let mp={};
+for(let i=0;i<arr.length;i++)
+	{
+		let articlelessItem=arr[i].replace(regexp,"").trim();
+		articlelessItem=articlelessItem.replace(/ /g," ");
+		articlelessarr.push(articlelessItem);
+		mp[articlelessItem]=arr[i];
+	}
+articlelessarr.sort();
+let ans[];
+for(let i of articlelessarr)
+	{
+		ans.push(mp[i]);
+	}
+console.log(ans);
